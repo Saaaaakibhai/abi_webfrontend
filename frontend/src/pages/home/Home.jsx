9f06b0260a49429bac3df2b4b34638c7794d0b99
Karib <abdurrahmanfarukkhan@gmail.com>
2024-09-24 12:56:26 +0600

    update 2

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,8 +1,6 @@
-import React from "react"
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>Home</div>
-    )
-}
-export default Home
+  return <div>Homefrom_KArib2</div>;
+};
+export default Home;
