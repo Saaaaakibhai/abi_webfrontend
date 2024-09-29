377ff53fc5b05a79102cb918ebf7ca6c25d28bbe
Karib <abdurrahmanfarukkhan@gmail.com>
2024-09-29 14:51:32 +0600

    news ticker scroll bar complete

---
+++ frontend/src/components/Newsticker.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../NewsTicker.css";
+
+const NewsTicker = () => {
+  return (
+    <div className="ticker-container">
+      <div className="ticker-text">
+        &lt;- Amanat Business Invest (ABI) -- Stay Ahead By Investment - &gt; ||
+        &lt;- Invest Your Earnings In A Halal Way &gt; || &lt;- Amanat Business
+        Invest (ABI) -- Stay Ahead By Investment - &gt;
+      </div>
+    </div>
+  );
+};
+
+export default NewsTicker;
