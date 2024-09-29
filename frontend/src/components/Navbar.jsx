53b191a6632daf2c54de39018e8c93a5442f81b2
Karib <abdurrahmanfarukkhan@gmail.com>
2024-09-29 14:56:12 +0600

    nav bar animation complete

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,33 +1,38 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 // navbar all pages
 const navLists = [
-    { name: "Home", path: "/" },
-    { name: "About Us", path: "/aboutus" },
-    { name: "Invest", path: "/invest" },
-    { name: "Project", path: "/project" },
-    { name: "Contact", path: "/contact" },
-]
+  { name: "Home", path: "/" },
+  { name: "About Us", path: "/aboutus" },
+  { name: "Invest", path: "/invest" },
+  { name: "Project", path: "/project" },
+  { name: "Contact", path: "/contact" },
+];
 const Navbar = () => {
   return (
-    <header className='bg-accent py-6 border'>
-          <nav className='container mx-auto flex justify-between px-5'>
-              {/* Logo */}
-              <a href="/">
-                <img src="images.png" alt="" className='h-12'/>
-              </a>
-              <ul className='sm:flex hidden items-center gap-8'>
-                  {
-                      navLists.map((list, index)=>(
-                          <li>
-                              <NavLink to={`${list.path}`}>{list.name}</NavLink>
-                          </li>
-                      ))
-                  }
-              </ul>
-          </nav>
+    <header className="bg-accent py-6 border">
+      <nav className="container mx-auto flex justify-between px-5">
+        {/* Logo */}
+        <a href="/">
+          <img src="images.png" alt="" className="h-12" />
+        </a>
+        <ul className="sm:flex hidden items-center gap-8">
+          {navLists.map((list, index) => (
+            <li>
+              <NavLink
+                to={`${list.path}`}
+                className={({ isActive, isPending }) =>
+                  isActive ? "active" : ""
+                }
+              >
+                {list.name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
