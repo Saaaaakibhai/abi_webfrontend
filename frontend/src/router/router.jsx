46d0e20f7e06c3aa1ffe7a553a010995675ea0ae
Karib Khan <168514054+Karib-Khan@users.noreply.github.com>
2024-10-24 14:52:11 +0600

    Update router.jsx

--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import Home from "../pages/home/home";
+import Home from "../pages/home/Home";
 import AboutUs from "../pages/home/miniPage/AboutUs";
 import Projects from "../pages/home/miniPage/Projects";
 import Contacts from "../pages/home/miniPage/Contacts";
