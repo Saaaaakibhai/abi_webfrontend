eb75860201baed9daff78131ab12a1814a181368
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2024-10-25 23:47:40 +0600

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './router/router.jsx'
+// index.jsx or App.jsx (wherever your entry point is)
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
+import router from './router/router.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <BrowserRouter basename="/abi_webfrontend/">
+      <RouterProvider router={router} />
+    </BrowserRouter>
+  </StrictMode>
+);
