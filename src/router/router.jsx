f7d1b9766bc07932ab6664135661b047bbe7bbd2
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2024-10-26 00:35:22 +0600

    Update

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import Home from "../pages/home/Home"; 
+import Home from "../pages/home/Home";
 import AboutUs from "../pages/home/miniPage/AboutUs";
 import Projects from "../pages/home/miniPage/Projects";
 import Contacts from "../pages/home/miniPage/Contacts";
@@ -8,30 +8,22 @@ import Invest from "../pages/home/miniPage/Invest";
 
 const router = createBrowserRouter([
   {
-    path: "/abi_webfrontend/",
+    path: "/",
     element: <App />,
     children: [
       {
-        path: "/", // Home route
+        path: "/",
         element: <Home />,
       },
       {
-        path: "aboutus",
+        path: "/aboutus",
         element: <AboutUs />,
       },
-      {
-        path: "project",
-        element: <Projects />,
-      },
-      {
-        path: "contact",
-        element: <Contacts />,
-      },
-      {
-        path: "invest",
-        element: <Invest />,
-      },
+      { path: "/project", element: <Projects /> },
+      { path: "/contact", element: <Contacts /> },
+      { path: "/invest", element: <Invest /> },
     ],
   },
 ]);
+
 export default router;
